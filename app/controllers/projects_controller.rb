7000303b863b5d7b5967bc8e3a9cdcee148db09e
Clay Ash <claycourtneyash@gmail.com>
2022-05-12 14:03:26 -0600

    Add new page and test, update form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,13 +2,23 @@ class ProjectsController < ApplicationController
   def index 
      @projects = Project.ordered_by_time
   end
-
+ 
   def show 
     @project = Project.find(params[:id])
   end
+  
+  def new   
+  end
 
-  def new 
-    
+  def create 
+    project = Project.new ({
+      name: params[:project][:name],
+      active: params[:project][:active], 
+      funding: params[:project][:funding]
+    })
+    project.save 
+
+    redirect_to '/projects'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/projects', to: 'projects#index'
-  get '/projects/:id', to: 'projects#show'
   get '/projects/new', to: 'projects#new'
+  get '/projects/:id', to: 'projects#show'
+  post '/projects', to: 'projects#create'
 
   get '/volunteers', to: 'volunteers#index'
   get '/volunteers/:id', to: 'volunteers#show'
